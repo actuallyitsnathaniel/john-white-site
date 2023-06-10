3cf6925e7e94546617b715eac23ddf281ca18884
nate@lightfeather.io <nate@lightfeather.io>
2023-06-10 16:30:02 -0700

    fixed about page dimensions

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -4,14 +4,14 @@ import oneSheet from '../../assets/images/john_white_one_sheet.png'
 // TODO: carousel-type component with all images shared from daniel
 function About() {
   return (
-    <div className="flex flex-wrap md:pt-14 justify-center items-center text-4xl text-white h-2/3">
-    <img
-      className="flex justify-center md:pt-8 px-12 mb-[5%] md:w-[66%]"
-      alt="john-white-one-sheet"
-      aria-label="john-white-one-sheet"
-      src={oneSheet}
-  />
-    </div>
+    <div className="flex flex-wrap md:pt-14 justify-center items-center text-4xl text-white h-2/3 md:h-fit">
+      <img
+        className="flex justify-center md:pt-8 px-12 mb-[5%] md:w-[66%]"
+        alt="john-white-one-sheet"
+        aria-label="john-white-one-sheet"
+        src={oneSheet}
+    />
+      </div>
   );
 }
 
