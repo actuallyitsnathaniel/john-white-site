966aa06033827e268cdf9c5c5fbd22b0b235011f
nate@lightfeather.io <nate@lightfeather.io>
2023-06-20 21:19:43 -0700

    attempting youube embed

--- src/components/music-embeds/apple-music/index.js
+++ src/components/music-embeds/apple-music/index.js
@@ -12,7 +12,7 @@ const AppleMusicEmbed = () => {
       }
       frameBorder={0}
       sandbox={
-        "allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+        "allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation-by-user-activation"
       }
       allow={"autoplay *; encrypted-media *; fullscreen *; clipboard-write"}
     />
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 import JohnWhiteLogo from "../../assets/images/icons/john-white-logo";
 import { MusicLinks } from ".//../../components/music-embeds";
 
 const Home = () => {
+
+  const [youtubeID,] = useState('cuzc9dn4sx4')
+
   return (
     <div className="text-white justify-items-center">
       <div className="pt-28"/>
@@ -11,7 +14,15 @@ const Home = () => {
         <JohnWhiteLogo />
         <p className="text-center text-8xl bottom-0 animate-bounce">↡</p>
       </div>
-      
+      <iframe 
+        width="560" 
+        height="315"
+        className="mx-auto" 
+        src={`https://www.youtube.com/embed/${youtubeID}`}
+        title="youtube video player" 
+        allow="accelerometer; autoplay; allow-scripts; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;" 
+        allowFullScreen
+      />
       <div>
         <MusicLinks />
       </div>
