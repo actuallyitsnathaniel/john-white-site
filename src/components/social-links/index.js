58f2fee85ac3d8665bb7f1d7bd19673fc5d32493
nate@lightfeather.io <nate@lightfeather.io>
2023-06-21 09:42:58 -0700

    fixing social links on mobile

--- src/components/social-links/index.js
+++ src/components/social-links/index.js
@@ -10,7 +10,7 @@ export const SocialLinks = () => {
         className={`${document.URL.includes("links") && "hidden"}
           ${document.URL.includes("about") && "hidden"}`}
       >
-      <div className={"flex justify-center mb-2.5 transition-translate duration-100 hover:delay-150 md:fixed md:bottom-0 bottom-auto md:left-0 left-auto lg:justify-start md:pt-4 sm:h-full md:h-auto translate-y-2 lg:translate-y-4 lg:hover:translate-y-1"}>
+      <div className={"flex flex-wrap mx-auto w-3/4 justify-center mb-2.5 transition-translate duration-100 hover:delay-150 md:fixed md:bottom-0 bottom-auto md:left-0 left-auto lg:justify-start md:pt-4 sm:h-full md:h-auto translate-y-2 lg:translate-y-4 lg:hover:translate-y-1"}>
         <SpotifyLink />
         <AppleMusic />
         <YoutubeLogo />
