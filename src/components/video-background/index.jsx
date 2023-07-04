f1a9701811d32c6b3b27aaf4098637cebbc172c4
nate@lightfeather.io <nate@lightfeather.io>
2023-07-03 22:02:37 -0700

    cleanest bg you ever did see

--- src/components/video-background/index.jsx
+++ src/components/video-background/index.jsx
@@ -1,24 +1,22 @@
-import React from 'react';
+import React from "react";
 
 import JohnWhiteVideo from "../../assets/videos/johnwhite-video-bg.mp4";
 
 const VideoBackground = () => {
   return (
-    <div className="video-wrapper">
-      <video
-        src={JohnWhiteVideo}
-        height={"110%"}
-        width={"auto"}
-        id="video"
-        rel="preload"
-        autoPlay
-        loop
-        muted
-        playsInline
-        disablePictureInPicture
-        className="video"
-      />
-    </div>
+    <video
+      src={JohnWhiteVideo}
+      height={"110%"}
+      width={"auto"}
+      id="video"
+      rel="preload"
+      autoPlay
+      loop
+      muted
+      playsInline
+      disablePictureInPicture
+      className="video"
+    />
   );
 };
 
