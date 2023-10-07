ce8133450dd5ac89c62826520f8af7f2ff9408dd
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2023-10-07 12:51:32 -0700

    removed strapi

--- strapi/src/api/album/routes/album.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * album router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::album.album');
--- strapi/src/api/album/services/album.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * album service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::album.album');
