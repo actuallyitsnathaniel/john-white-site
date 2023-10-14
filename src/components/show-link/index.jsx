ad9620bdc0cd4d445a2ed23ca198a781371ec096
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2023-10-14 10:21:15 -0700

    fixed spacing in show link

--- src/components/show-link/index.jsx
+++ src/components/show-link/index.jsx
@@ -4,11 +4,14 @@ const ShowLink = (props) => {
   return (
     <a href={props.link} className="text-center p-3">
       <div className="sm:mb-20 md:mb-10">
-        <p className="text-xl font-semibold">{props.artists}</p>
-        <p className="font-semibold">{props.date}</p>
-        <p className="p-0 m-0 text-xl">{props.venue}</p>
+        <span className="">
+          <p className="text-xl font-semibold">{props.artists}</p>
+          <p className="font-semibold">{props.date}</p>
+          <p className="p-0 m-0 text-xl">{props.venue}</p>
+        </span>
+
         <img
-          className="mx-auto transition duration-75 h-80 my-4 hover:-translate-y-1 hover:scale-115"
+          className="py-4 object-scale-down transition w-[375px] duration-75 px-4 hover:-translate-y-1 hover:scale-115"
           src={props.thumbnail}
           alt={props.alt}
         />
