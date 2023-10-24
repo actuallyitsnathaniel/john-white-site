6df3f5c21b97c0dc4d744e7b4bdad27397d0990c
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2023-10-23 17:12:42 -0700

    reworked about first letter

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -18,25 +18,28 @@ const About = () => {
     <div className="md:flex md:flex-wrap -mt-10 md:mt-0 md:pt-14 justify-between text-2xl text-white">
       <div className="md:flex md:flex-wrap p-8">
         <div className="flex flex-wrap md:justify-between md:pb-10">
-          <div className="lg:px-5 pb-10 lg:pb-0 text-justify lg:w-1/2 md:text-3xl first-line:italic">
+          <div
+            className="lg:px-5 pb-10 lg:pb-0 text-justify lg:w-1/2 md:text-3xl first-line:italic first-line:tracking-widest
+                        first-letter:text-7xl first-letter:mb-1 first-letter:font-bold first-letter:text-white
+                        first-letter:mr-4 first-letter:float-left"
+          >
             {
               // TODO: migrate this to strapi.
               //  figure out how to add spans there as well to maintain the capitalization look.
             }
-            <span className="font-extrabold text-4xl md:text-6xl">J</span>ohn{" "}
-            <span className="font-extrabold text-4xl md:text-6xl">W</span>hite
-            is an up-and-coming artist born and raised in South LA. Exploring
-            the sounds of the bands he listened to with his father, John writes
-            about love, loss, and acceptance in a way that can be understood by
-            all. At the end of the day. his message is about embracing one
-            another regardless of race, class, sexuality, and coming together
-            through music to understand that we are not alone. As he prepares to
-            roll out his debut album, John looks forward to bringing his music
-            into a live setting. One of his favorite memories is seeing Oasis
-            with his dad and taking in how much it meant to experience the songs
-            he knew well, but with someone that loved him deeply. He hopes to
-            bring audiences that same love and comfort. His debut album, Through
-            the Trees, will be released on September 22, 2023.
+            John White is an up-and-coming artist born and raised in South LA.
+            Exploring the sounds of the bands he listened to with his father,
+            John writes about love, loss, and acceptance in a way that can be
+            understood by all. At the end of the day. his message is about
+            embracing one another regardless of race, class, sexuality, and
+            coming together through music to understand that we are not alone.
+            As he prepares to roll out his debut album, John looks forward to
+            bringing his music into a live setting. One of his favorite memories
+            is seeing Oasis with his dad and taking in how much it meant to
+            experience the songs he knew well, but with someone that loved him
+            deeply. He hopes to bring audiences that same love and comfort. His
+            debut album, Through the Trees, will be released on September 22,
+            2023.
           </div>
           {/* 
           TODO: refine carousel to be this.
