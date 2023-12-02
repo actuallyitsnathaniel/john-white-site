fb03e2bbf28d7cc9b92e082bf844167bc4cdd989
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2023-12-02 08:39:37 -0800

    linting platform links

--- src/components/discography/disc/platform-links.jsx
+++ src/components/discography/disc/platform-links.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 // Icons
 import appleMusic from "../../../assets/images/icons/music-platforms/apple-music.svg";
 import spotify from "../../../assets/images/icons/music-platforms/spotify.svg";
-import soundcloud from "../../../assets/images/icons/music-platforms/soundcloud.svg"
+import soundcloud from "../../../assets/images/icons/music-platforms/soundcloud.svg";
 import youtube from "../../../assets/images/icons/music-platforms/youtube.svg";
 
 const MusicPlatformLinks = (props) => {
@@ -15,52 +15,57 @@ const MusicPlatformLinks = (props) => {
       } justify-items-center items-center -translate-x-1 -translate-y-1 p-4
       `}
     >
-      <a 
+      <a
         href={props.spotifyLink}
-        className={`${props.spotifyLink === '' && 'hidden'}`}  
-        rel="noopener noreferrer" 
-        target="_blank">
+        className={`${props.spotifyLink === "" && "hidden"}`}
+        rel="noopener noreferrer"
+        target="_blank"
+      >
         <img
           className="transition-all duration-75 hover:scale-110"
-          height={'75px'}
+          height={"75px"}
           alt="spotify-link"
           src={spotify}
         />
       </a>
 
-      <a 
+      <a
         href={props.appleMusicLink}
-        className={`${props.appleMusicLink === '' && 'hidden'}`} 
+        className={`${props.appleMusicLink === "" && "hidden"}`}
         rel="noopener noreferrer"
-        target="_blank">
+        target="_blank"
+      >
         <img
           className="transition-all duration-75 hover:scale-110"
-          height={'75px'}
+          height={"75px"}
           alt="apple-music-link"
           src={appleMusic}
         />
       </a>
 
-      <a href={props.soundcloudLink} 
-        className={`${props.soundcloudLink === '' && 'hidden'}`} 
-        rel="noopener noreferrer" 
-        target="_blank">
+      <a
+        href={props.soundcloudLink}
+        className={`${props.soundcloudLink === "" && "hidden"}`}
+        rel="noopener noreferrer"
+        target="_blank"
+      >
         <img
           className={`transition-all duration-75 scale-110 hover:scale-125`}
-          height={'75px'}
+          height={"75px"}
           alt="soundcloud-link"
           src={soundcloud}
         />
       </a>
 
-      <a 
+      <a
         href={props.youtubeLink}
-        className={`${props.youtubeLink === '' && 'hidden'}`} 
-        rel="noopener noreferrer" 
-        target="_blank">
+        className={`${props.youtubeLink === "" && "hidden"}`}
+        rel="noopener noreferrer"
+        target="_blank"
+      >
         <img
           className="transition-all duration-75 hover:scale-110"
-          height={'75px'}
+          height={"75px"}
           alt="youtube-link"
           src={youtube}
         />
