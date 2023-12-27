ef2e3bec3392a3dca6066249623af1b6124ffac0
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2023-12-26 19:53:24 -0800

    added about page

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,5 +1,21 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface PageAbout extends Schema.Component {
+  collectionName: 'components_page_abouts';
+  info: {
+    displayName: 'About';
+  };
+  attributes: {
+    Description: Attribute.Text & Attribute.Required;
+    Pictures: Attribute.Media & Attribute.Required;
+    PressHighlights: Attribute.Blocks;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'page.about': PageAbout;
+    }
+  }
 }
