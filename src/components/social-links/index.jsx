2b370210f56e0667a2850aea7c2d49d8f5064a63
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2023-12-29 21:26:46 -0800

    updated social-links padding

--- src/components/social-links/index.jsx
+++ src/components/social-links/index.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom'
+import React from "react";
+import { useLocation } from "react-router-dom";
 
 import InstagramLogo from "./instagram-link";
 import GmailLogo from "./gmail-link";
@@ -9,12 +9,18 @@ import YoutubeLogo from "./youtube-link";
 
 export const SocialLinks = () => {
   let location = useLocation();
-  const isHidden = (location.pathname.includes("links") || location.pathname.includes("about")) && "hidden";
+  const isHidden =
+    (location.pathname.includes("links") ||
+      location.pathname.includes("about")) &&
+    "hidden";
 
   return (
-    <div
-      className={`${isHidden}`}>
-      <div className={"flex flex-wrap mx-auto w-3/4 justify-center mb-2.5 transition-translate duration-100 hover:delay-150 md:fixed md:bottom-0 bottom-auto md:left-0 left-auto lg:justify-start md:pt-4 sm:h-full md:h-auto translate-y-2 lg:translate-y-4 lg:hover:translate-y-1"}>
+    <div className={`${isHidden}`}>
+      <div
+        className={
+          "flex flex-wrap mx-auto pr-5 justify-center mb-2.5 transition-translate duration-100 hover:delay-150 md:fixed md:bottom-0 bottom-auto md:left-0 left-auto lg:justify-start md:pt-4 sm:h-full md:h-auto translate-y-2 lg:translate-y-4 lg:hover:translate-y-1"
+        }
+      >
         <SpotifyLink />
         <AppleMusic />
         <YoutubeLogo />
@@ -23,4 +29,4 @@ export const SocialLinks = () => {
       </div>
     </div>
   );
-}
+};
