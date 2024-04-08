173d22774098f39362084cebefb38ae3206ad2ff
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2024-04-08 15:43:58 -0700

    show height fix

--- src/pages/shows/index.jsx
+++ src/pages/shows/index.jsx
@@ -3,7 +3,7 @@ import writersBlockHotelCafe from "../../assets/images/thumbnails/hotelcafe_writ
 
 const Shows = () => {
   return (
-    <main className="flex flex-wrap min-h-full font-semibold md:mt-16 text-center px-3 text-white justify-center text-3xl">
+    <main className="flex flex-wrap h-screen font-semibold md:mt-16 text-center px-3 text-white justify-center text-3xl">
       <div className="md:p-3 lowercase">
         <ShowLink
           link="https://new.hotelcafe.com/event/writers-block-147/"
