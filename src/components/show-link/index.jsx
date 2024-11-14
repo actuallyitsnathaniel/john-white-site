c3c523cf4dade09d5604d0a2829c5fec55c8adf1
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2024-11-13 18:04:13 -0800

    date behavior is weid

--- src/components/show-link/index.jsx
+++ src/components/show-link/index.jsx
@@ -14,30 +14,30 @@ const ShowLink = (props) => {
   const showDate = new Date(`${props.date}`.replace(/th|st|rd/g, ""))
     .toISOString()
     .split("T")[0];
-
+  console.log(date, showDate);
   return (
-    date <= showDate && (
-      <a href={props.link} className="text-center p-3">
-        <div className="transition duration-100 sm:mb-20 md:mb-10 hover:-translate-y-1 hover:scale-115 object-scale-down">
-          <span>
-            <p className="text-xl font-semibold">{props.artists}</p>
-            <p className="font-semibold">{props.date}</p>
-            <p className="p-0 m-0 text-xl">{props.venue}</p>
-            <p className="p-0 m-0 text-xl">{props.venueAddress}</p>
-          </span>
+    // date <= showDate && (
+    <a href={props.link} className="text-center p-3">
+      <div className="transition duration-100 sm:mb-20 md:mb-10 hover:-translate-y-1 hover:scale-115 object-scale-down">
+        <span>
+          <p className="text-xl font-semibold">{props.artists}</p>
+          <p className="font-semibold">{props.date}</p>
+          <p className="p-0 m-0 text-xl">{props.venue}</p>
+          <p className="p-0 m-0 text-xl">{props.venueAddress}</p>
+        </span>
 
-          <img
-            className="p-4 max-w-[375px] max-h-[400px] mx-auto object-scale-down"
-            src={props.thumbnail}
-            alt={props.alt}
-          />
-          {props.price && (
-            <span className="font-semibold text-4xl ">{props.price}</span>
-          )}
-          {props.venmoPrice && <VenmoLink price={props.venmoPrice} />}
-        </div>
-      </a>
-    )
+        <img
+          className="p-4 max-w-[375px] max-h-[400px] mx-auto object-scale-down"
+          src={props.thumbnail}
+          alt={props.alt}
+        />
+        {props.price && (
+          <span className="font-semibold text-4xl ">{props.price}</span>
+        )}
+        {props.venmoPrice && <VenmoLink price={props.venmoPrice} />}
+      </div>
+    </a>
   );
+  // );
 };
 export default ShowLink;
