1a128f2e706d22b649783a39d31dd0b74ffcb12d
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2024-11-15 08:29:36 -0800

    video bg fix

--- src/components/video-background/index.jsx
+++ src/components/video-background/index.jsx
@@ -6,13 +6,12 @@ const VideoBackground = () => {
       height={"100%"}
       width={"auto"}
       id="video"
-      // rel="preload"
       autoPlay
       loop
       muted
       playsInline
       disablePictureInPicture
-      className="brightness-[25%] blur saturate-100 w-full h-full object-cover scale-[100%] fixed z-[-1] bg-black overflow-clip md:overflow-hidden flex md:align-middle md:content-center"
+      className="brightness-[50%] blur saturate-100 w-full h-full object-cover fixed z-[-1] bg-black overflow-clip md:overflow-hidden flex md:align-middle md:content-center"
     >
       <source src={JohnWhite42VideoMP4} type="video/MP4" />
     </video>
