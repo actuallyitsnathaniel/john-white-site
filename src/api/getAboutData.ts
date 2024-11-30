e65a756dc326e7056569f94a769284850bac55a5
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2024-11-29 19:12:54 -0800

    removing a lot of stuff, some about page works now

--- src/api/getAboutData.ts
+++ src/api/getAboutData.ts
@@ -1,29 +1,7 @@
 import { prod_url, GET_options } from "./util";
 
-const getPressHighlights = await fetch(
-  `${prod_url}/api/press-highlights?populate=*`,
-  GET_options
-)
-  .then((response) => response.json())
-  .then((data) => data.data)
-  .catch((error) => console.log(error.stack));
-
-export const PressHighlightEntries = getPressHighlights.map((entry) => {
-  entry = entry.attributes;
-
-  return { entry };
-});
-
-const getContacts = await fetch(
-  `${prod_url}/api/contacts?populate=*`,
-  GET_options
-)
-  .then((response) => response.json())
-  .then((data) => data.data)
-  .catch((error) => console.log(error.stack));
-
-export const Contacts = getContacts.map((entry) => {
-  entry = entry.attributes;
-
-  return { entry };
-});
+export const getAboutPage = async () =>
+  await fetch(`${prod_url}/api/about?populate=AboutPhotos.photos`, GET_options)
+    .then((response) => response.json())
+    .then((data) => data.data)
+    .catch((error) => console.log(error.stack));
--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -1,71 +1,90 @@
-import profile1 from "../../assets/images/profiles/profile-1_2.jpg";
-import profile2 from "../../assets/images/profiles/profile-2.jpeg";
-import profile3 from "../../assets/images/profiles/profile-3_2.jpg";
-import profile4 from "../../assets/images/profiles/profile-4.jpeg";
-import profile5 from "../../assets/images/profiles/profile-5.jpeg";
-
 import InstagramLogo from "../../components/social-links/instagram-link";
 import SpotifyLink from "../../components/social-links/spotify-link";
 import AppleMusic from "../../components/social-links/apple-music-link";
 import YoutubeLogo from "../../components/social-links/youtube-link/index.js";
 
 import PressHighlight from "../../components/press-highlight/index.js";
+import { getAboutPage } from "../../api/getAboutData";
+import { useEffect, useState } from "react";
+import Loading from "../../components/loading";
+
+type DescriptionType = {
+  type: string;
+  children: { text: string; type: string }[];
+};
+
+type PhotoType = {
+  id: number;
+  name: string;
+  url: string;
+  formats: {
+    thumbnail: { url: string };
+  };
+};
+
+type AboutData = {
+  id: number;
+  documentId: string;
+  title: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  Description: DescriptionType[];
+  AboutPhotos: {
+    id: number;
+    photos: PhotoType[];
+  };
+};
 
 const About = () => {
-  return (
+  const [isLoading, setIsLoading] = useState(true);
+  const [about, setAbout] = useState<AboutData>();
+
+  useEffect(() => {
+    const fetchAboutPage = async () => {
+      setIsLoading(true);
+      try {
+        const response = await getAboutPage();
+        setAbout(response);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchAboutPage();
+  }, []);
+  console.log(about);
+
+  return isLoading ? (
+    <Loading />
+  ) : (
     <div className="flex flex-col mt-16 justify-center text-2xl text-white">
       <div className="flex flex-wrap p-10 mx-auto">
         <div className="flex flex-wrap justify-around md:pb-10 items-center">
-          <div className="lg:px-14 pb-10 text-justify lg:w-[55%] md:text-3xl first-line:italic">
-            <span className="font-extrabold text-4xl md:text-6xl">J</span>
-            ohn&nbsp;
-            <span className="font-extrabold text-4xl md:text-6xl">W</span>hite
-            is an up-and-coming artist born and raised in South LA. Exploring
-            the sounds of the bands he listened to with his father, John writes
-            about love, loss, and acceptance in a way that can be understood by
-            all. At the end of the day. his message is about embracing one
-            another regardless of race, class, sexuality, and coming together
-            through music to understand that we are not alone. As he prepares to
-            roll out his debut album, John looks forward to bringing his music
-            into a live setting. One of his favorite memories is seeing Oasis
-            with his dad and taking in how much it meant to experience the songs
-            he knew well, but with someone that loved him deeply. He hopes to
-            bring audiences that same love and comfort. His latest record, For a
-            While, was released on March 22, 2024.
+          <div
+            className="lg:px-14 pb-10 text-justify lg:w-[55%] md:text-3xl 
+          first-letter:font-extrabold first-letter:text-4xl
+          first-line:italic"
+          >
+            {`${about?.Description[0].children[0].text}`}
           </div>
           {/* 
-          TODO: refine carousel to be this.
-          https://stackoverflow.com/questions/59198952/using-document-queryselector-in-react-should-i-use-refs-instead-how
-           */}
+        TODO: refine carousel to be this.
+        https://stackoverflow.com/questions/59198952/using-document-queryselector-in-react-should-i-use-refs-instead-how
+         */}
           <div
             id="carousel"
             className="flex rounded-xl overflow-scroll snap-x snap-mandatory h-fit lg:w-[42vw] mx-auto"
           >
-            <img
-              className="object-cover snap-center"
-              src={profile3}
-              alt="profile-img-3"
-            />
-            <img
-              className="object-cover snap-center"
-              src={profile4}
-              alt="profile-img-4"
-            />
-            <img
-              className="object-cover snap-center"
-              src={profile1}
-              alt="profile-img-1"
-            />
-            <img
-              className="object-cover snap-center"
-              src={profile2}
-              alt="profile-img-2"
-            />
-            <img
-              className="object-cover snap-center"
-              src={profile5}
-              alt="profile-img-5"
-            />
+            {about?.AboutPhotos.photos.map((photo, i) => (
+              <img
+                className="object-cover snap-center"
+                key={i}
+                src={photo.url}
+                alt={`"profile-img-${photo.id}"`}
+              />
+            ))}
           </div>
         </div>
         <br />
