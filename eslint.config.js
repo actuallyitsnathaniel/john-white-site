44a243833413eeeaf623d7fcda4af6f70bfffb3d
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2025-10-30 19:45:06 -0700

    eslint update

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,9 @@
 import js from '@eslint/js';
 import tseslint from '@typescript-eslint/eslint-plugin';
 import tsparser from '@typescript-eslint/parser';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
   {
@@ -9,6 +12,11 @@ export default [
       ecmaVersion: 'latest',
       sourceType: 'module',
       parser: tsparser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
       globals: {
         fetch: 'readonly',
         console: 'readonly',
@@ -21,10 +29,25 @@ export default [
     },
     plugins: {
       '@typescript-eslint': tseslint,
+      'react': react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
     rules: {
       ...js.configs.recommended.rules,
       ...tseslint.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
     },
   },
 ];
